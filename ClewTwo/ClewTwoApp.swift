83ff8ff51af351dc13e04de7c7180076afe1abf6
Evelyn Kessler <molly@healthea.org>
2023-03-19 20:23:11 -0400

    Initial Commit

---
+++ ClewTwo/ClewTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClewTwoApp.swift
+//  ClewTwo
+//
+//  Created by Evelyn on 3/19/23.
+//
+
+import SwiftUI
+
+@main
+struct ClewTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
